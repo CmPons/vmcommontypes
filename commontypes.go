827d6d3eafcb2a3ada3d66097d41ca92bfe93570
Chris Pons <cmpons@gmail.com>
2022-09-04 15:56:57 +0200

    Test

--- commontypes.go
+++ commontypes.go
@@ -3,7 +3,6 @@ package commontypes
 type Command struct {
 	Identifier  string
 	Description string
-
 	CommandString string            // This is the full command sent to the terminal ex. "ls -a". Use ${Arg1} for variable substitution
 	Variables     map[string]string // This is a list of variables for substitution
 }
