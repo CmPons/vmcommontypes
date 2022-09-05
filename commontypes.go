bf1b3f5afc5b358d3ce316e2acd78d378b18d395
Chris Pons <cmpons@gmail.com>
2022-09-05 19:13:44 +0200

    Commands now just hold an array of commands as variables

--- commontypes.go
+++ commontypes.go
@@ -1,8 +1,20 @@
 package commontypes
 
+import "fmt"
+
 type Command struct {
 	Identifier    string
 	Description   string
-	CommandString string            // This is the full command sent to the terminal ex. "ls -a". Use ${Arg1} for variable substitution
-	Variables     map[string]string // This is a list of variables for substitution
+	CommandString string    // This is the full command sent to the terminal ex. "ls -a". Use ${Arg1} for variable substitution
+	Variables     []Command // This is a list of variables for substitution
+}
+
+func (c *Command) Usage() string {
+	ret := c.Identifier + "\t" + c.Description
+
+	for _, command := range c.Variables {
+		ret += "\t" + command.Usage()
+	}
+
+	return ret
 }
